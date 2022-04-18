891d3b107c16fe517cfb40984f36672c0286d1d2
tgtiburon <tg.tiburon@gmail.com>
2022-04-18 16:21:42 -0400

    practice modules

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,22 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 3.5;
+        String c = "cat";
+
+        // calculated variable sum is double
+        double sum = a + b;
+
+        // calculated variable concat is a string
+        String concat = a + c;
+
+        // Value of sum is 7.5
+        System.out.println(sum);
+
+        // value of concat is "4cat"
+        System.out.println(concat);
+
+    }
+};
